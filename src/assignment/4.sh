f325386768f8b902fa90551280b83b7e90a6948b
A Omkar G Prabhu <110335801+Prabhuomkar9@users.noreply.github.com>
2023-10-10 23:31:49 +0530

    Update 4.sh

--- src/assignment/4.sh
+++ src/assignment/4.sh
@@ -1,24 +1,47 @@
 #!\bin\bash
 
-ls -l
-echo "___________________"
-ls -l | tail -n +2
-echo "___________________"
 list=`ls -l | tail -n +2`
-echo $list
-echo "-------------------"
 
 count=0
-flag=false
+flag=true
+echo "Following files have READ, WRITE, EXECUTE permissions for user"
 for field in $list
 do
     if [ $count -eq 0 ]
     then
-        echo `cut -c 1-2 $field`
-        echo $field
+        if [ `echo $field | cut -b 1` = "d" ]
+        then
+            flag=false
+        elif [ `echo $field | cut -b 2-4` != "rwx" ]
+        then
+            flag=false
+        fi
+        count=`expr $count + 1`
     elif [ $count -eq 8 ]
     then
-        echo $field
+        if [ $flag = true ]
+        then
+            echo $field
+        fi
+        count=0
+        flag=true
+    else
+        count=`expr $count + 1`
     fi
-    count=`expr $count + 1`
 done
+
+<<OUTPUT
+ls -l
+total 7
+drwxr-xr-x 1 runner runner   0 Oct 10 17:59 d1
+drwxr-xr-x 1 runner runner   0 Oct 10 17:59 d2
+drwxr-xr-x 1 runner runner   0 Oct 10 17:59 d3
+-rwxr--r-- 1 runner runner 594 Oct 10 17:58 main.sh
+--w------- 1 runner runner   0 Oct 10 17:45 test1
+-rw-r--r-- 1 runner runner   0 Oct 10 17:47 test2
+-rwxr-xr-x 1 runner runner   0 Oct 10 17:47 test3
+
+sh main.sh
+Following files have READ, WRITE, EXECUTE permissions for user
+test3
+OUTPUT
